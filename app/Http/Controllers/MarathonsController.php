f8cfc5e2446b9825b0bf964c3e75fb304dec1660
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2016-10-21 10:34:19 +0300

    changes

--- app/Http/Controllers/MarathonsController.php
+++ app/Http/Controllers/MarathonsController.php
@@ -47,10 +47,9 @@ class MarathonsController extends Controller
             $orderItem->title = $marathon->name;
             $orderItem->price = $marathon->price * $attendees1;
             $orderItem->order_id = $order->id;
-            $orderItem->created_at = Carbon::now('Europe/Istanbul');
+
             $orderItem->save();
 
-            $order->updated_at = Carbon::now('Europe/Istanbul');
             $order->save();
         }
         if ($attendees2 > 0) {
@@ -62,10 +61,10 @@ class MarathonsController extends Controller
             $orderItem->title = $marathon->name;
             $orderItem->price = $marathon->price * $attendees2;
             $orderItem->order_id = $order->id;
-            $orderItem->created_at = Carbon::now('Europe/Istanbul');
+            
             $orderItem->save();
 
-            $order->updated_at = Carbon::now('Europe/Istanbul');
+        
             $order->save();
         }
         if ($attendees3 > 0) {
@@ -77,10 +76,10 @@ class MarathonsController extends Controller
             $orderItem->title = $marathon->name;
             $orderItem->price = $marathon->price * $attendees3;
             $orderItem->order_id = $order->id;
-            $orderItem->created_at = Carbon::now('Europe/Istanbul');
+            
             $orderItem->save();
 
-            $order->updated_at = Carbon::now('Europe/Istanbul');
+        
             $order->save();
         }
 
