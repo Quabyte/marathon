6f22e284d885aa86728c62e22745d563055ea566
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2016-10-26 15:21:19 +0300

    Added Powered By Açıkgişe

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,11 @@
 <div class="siteMap">
 	<div class="container">
-		
+		<div class="pull-right">
+			<p style="color: #f5f5f5;
+				float: left;
+    			margin-right: 10px;
+    margin-top: 2px;">Powered by</p>
+        	<img class="img-responsive" src="{{ asset('images/logo-negatif.jpg') }}" width="100">
+		</div>
 	</div>
 </div>
